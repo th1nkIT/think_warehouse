0b45035713f1f63afc59ddddfcfcfe64887dc3a9
wali98-rgb <wyyuddin@gmail.com>
2024-01-09 21:32:17 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  return (
+    <>
+      <div>snaifn</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dashboard() {
+    return (
+        <nav>
+            <a href="">aaa</a>
+        </nav>
+    );
+}
